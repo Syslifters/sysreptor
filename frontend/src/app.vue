e7a5929944e63fe8f10b4f2e47425dbc81984dd5
Michael Wedl <michael@syslifters.com>
2023-12-13 10:38:09 +0100

    Clean up

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -41,35 +41,3 @@ const theme = computed(() => {
   }
 });
 </script>
-
-<!-- TODO: permissions: disable instead of hiding 
-* [x] Archived Projects in menu
-* [x] Archive button in Project menu
-* [x] Public key page in user menu
-* [x] SSO Identities in user menu
-* [x] create design
-* [x] import design
-* [x] create template
-* [x] import template
-* [x] create user
-* [x] guest permissions
-  * [x] apiSettings: provide guest permission settings to frontend
-  * [x] create project
-  * [x] import project
-  * [x] change project settings
-  * [x] delete project
-  * [x] customize design in project
-  * [x] finish project
-  * [x] archive project
-  * [x] copy project
-* [x] EditToolbar
-  * [x] archive button
-  * [x] context actions
-  * [x] save button
-  * [x] delete button
-* [x] version history: temporary hover over sub-drawer
-* [ ] add "/PRO" badge when in community edition for disabled features
-  * [x] buttons and menus
-  * [ ] sudo button in main drawer ???
-  * [ ] sudo in user menu ???
--->
