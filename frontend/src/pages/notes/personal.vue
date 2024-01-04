3724ea87af65aa5f1325f7469e3d4d1bb0897999
Michael Wedl <michael@syslifters.com>
2024-01-04 09:07:02 +0100

    Fix note subheader spacing

--- frontend/src/pages/notes/personal.vue
+++ frontend/src/pages/notes/personal.vue
@@ -1,7 +1,7 @@
 <template>
   <split-menu v-model="localSettings.notebookInputMenuSize" :content-props="{ class: 'pa-0 h-100' }">
     <template #menu>
-      <v-list density="compact" class="pt-0 pb-0 h-100 d-flex flex-column">
+      <v-list density="compact" class="pb-0 h-100 d-flex flex-column">
         <v-list-subheader>
           <span>Personal Notes</span>
           <s-btn-icon
@@ -118,5 +118,9 @@ const updateNoteOrder = debounce(async (notes: NoteGroup<UserNote>) => {
   display: inline-block;
   margin: 0;
   padding: 0;
+}
+
+:deep(.v-list-subheader) {
+  padding-left: 0.5em !important;
 }
 </style>
