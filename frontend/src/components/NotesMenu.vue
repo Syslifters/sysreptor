9ec3df6f7c428a5524d80d745bb9a5b3e7cd1862
Michael Wedl <michael@syslifters.com>
2024-01-25 11:19:22 +0100

    Drag and drop notes import

--- frontend/src/components/NotesMenu.vue
+++ frontend/src/components/NotesMenu.vue
@@ -1,64 +1,71 @@
 <template>
-  <v-list density="compact" class="pb-0 h-100 d-flex flex-column">
-    <v-list-subheader>
-      <span>{{ props.title || 'Notes' }}</span>
-      <s-btn-icon
-        @click="createNoteBtnRef?.click()"
-        :disabled="!canCreate"
-        size="small"
-        variant="flat"
-        color="secondary"
-        density="compact"
-        class="ml-2"
-      >
-        <v-icon icon="mdi-plus" />
-        <s-tooltip activator="parent" location="top">Add Note (Ctrl+J)</s-tooltip>
-      </s-btn-icon>
-      <v-spacer />
-      <s-btn-icon v-if="props.exportUrl || props.performImport" size="small" density="compact">
-        <v-icon icon="mdi-dots-vertical" />
-        <v-menu activator="parent" :close-on-content-click="false" location="bottom right" class="context-menu">
-          <v-list>
-            <btn-import 
-              v-if="props.performImport"
-              :import="performImport"
-              :disabled="props.readonly"
-              button-variant="list-item"
-            />
-            <btn-export
-              v-if="props.exportUrl"
-              button-text="Export All"
-              :export-url="props.exportUrl"
-              :name="props.exportName"
-              :disabled="props.readonly"
-            />
-          </v-list>
-        </v-menu>
-      </s-btn-icon>
-    </v-list-subheader>
-
-    <div class="flex-grow-1 overflow-y-auto">
-      <slot name="default" />
-    </div>
-
-    <div>
-      <v-divider />
-      <v-list-item>
-        <btn-confirm
-          ref="createNoteBtnRef"
-          :action="createNote"
+  <file-drop-area 
+    @drop="importBtnRef?.performImport($event)"
+    :disabled="props.readonly || !props.performImport" 
+    class="h-100"
+  >
+    <v-list density="compact" class="pb-0 h-100 d-flex flex-column">
+      <v-list-subheader>
+        <span>{{ props.title || 'Notes' }}</span>
+        <s-btn-icon
+          @click="createNoteBtnRef?.click()"
           :disabled="!canCreate"
-          :confirm="false"
-          button-text="Add"
-          button-icon="mdi-plus"
-          tooltip-text="Add Note (Ctrl+J)"
-          keyboard-shortcut="ctrl+j"
           size="small"
-          block
-        />
-      </v-list-item>
-    </div>
-  </v-list>
+          variant="flat"
+          color="secondary"
+          density="compact"
+          class="ml-2"
+        >
+          <v-icon icon="mdi-plus" />
+          <s-tooltip activator="parent" location="top">Add Note (Ctrl+J)</s-tooltip>
+        </s-btn-icon>
+        <v-spacer />
+        <s-btn-icon v-if="props.exportUrl || props.performImport" size="small" density="compact">
+          <v-icon icon="mdi-dots-vertical" />
+          <v-menu activator="parent" eager :close-on-content-click="false" location="bottom right" class="context-menu">
+            <v-list>
+              <btn-import 
+                v-if="props.performImport"
+                ref="importBtnRef"
+                :import="performImport"
+                :disabled="props.readonly"
+                button-variant="list-item"
+              />
+              <btn-export
+                v-if="props.exportUrl"
+                button-text="Export All"
+                :export-url="props.exportUrl"
+                :name="props.exportName"
+                :disabled="props.readonly"
+              />
+            </v-list>
+          </v-menu>
+        </s-btn-icon>
+      </v-list-subheader>
+
+      <div class="flex-grow-1 overflow-y-auto">
+        <slot name="default" />
+      </div>
+
+      <div>
+        <v-divider />
+        <v-list-item>
+          <btn-confirm
+            ref="createNoteBtnRef"
+            :action="createNote"
+            :disabled="!canCreate"
+            :confirm="false"
+            button-text="Add"
+            button-icon="mdi-plus"
+            tooltip-text="Add Note (Ctrl+J)"
+            keyboard-shortcut="ctrl+j"
+            size="small"
+            block
+          />
+        </v-list-item>
+      </div>
+    </v-list>
+  </file-drop-area>
 </template>
 
 <script setup lang="ts">
@@ -70,6 +77,8 @@ const props = defineProps<{
   exportName?: string;
   performImport?: (file: File) => Promise<void>;
 }>();
+
+const importBtnRef = ref();
 
 const createNoteBtnRef = ref();
 const canCreate = computed(() => !props.readonly && !!props.createNote);
