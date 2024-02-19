1723881ee1fc6e266b03fd7c8772cff7802a184f
Michael Wedl <michael@syslifters.com>
2024-02-19 08:27:38 +0100

    Add CWE terms of use

--- api/generate_notice.sh
+++ api/generate_notice.sh
@@ -18,6 +18,7 @@ allow_only="$allow_only;Python Software Foundation License"
 ignore="webencodings"
 ignore="$ignore pyphen"
 ignore="$ignore freetype2"
+ignore="$ignore cwe"
 
 pip3 install pip-licenses
 pip-licenses --allow-only "$allow_only" >/dev/null
@@ -705,6 +706,48 @@ Legal Terms
 
 
 --- end of FTL.TXT ---
+EOF
+)
+
+cwe=$(cat << EOF
+# Terms of Use
+CWE™ is free to use by any organization or individual for any research, 
+development, and/or commercial purposes, per these CWE Terms of Use. 
+Accordingly, The MITRE Corporation hereby grants you a non-exclusive, 
+royalty-free license to use CWE for research, development, and commercial purposes.
+Any copy you make for such purposes is authorized on the condition that 
+you reproduce MITRE's copyright designation and this license in any such copy. 
+CWE is a trademark of The MITRE Corporation. Please contact cwe@mitre.org 
+if you require further clarification on this issue.
+
+## Disclaimers
+By accessing information through this site you (as “the user”) hereby agrees 
+the site and the information is provided on an “as is” basis only without 
+warranty of any kind, express or implied, including but not limited to 
+implied warranties of merchantability, availability, accuracy, noninfringement, 
+or fitness for a particular purpose. Use of this site and the information 
+is at the user's own risk. The user shall comply with all applicable laws, 
+rules, and regulations, and the data source's restrictions, when using the site.
+
+By contributing information to this site you (as “the contributor”) hereby 
+represents and warrants the contributor has obtained all necessary permissions 
+from copyright holders and other third parties to allow the contributor to contribute, 
+and this site to host and display, the information and any such contribution, 
+hosting, and displaying will not violate any law, rule, or regulation. 
+Additionally, the contributor hereby grants all users of such information a perpetual,
+worldwide, non-exclusive, no-charge, royalty-free, irrevocable license to reproduce, 
+prepare derivative works of, publicly display, publicly perform, sublicense, 
+and distribute such information and all derivative works.
+
+The MITRE Corporation expressly disclaims any liability for any damages arising 
+from the contributor's contribution of such information, the user's use of the 
+site or such information, and The MITRE Corporation's hosting the tool and 
+displaying the information. The foregoing disclaimer specifically includes 
+but is not limited to general, consequential, indirect, incidental, exemplary, 
+or special or punitive damages (including but not limited to loss of income, 
+program interruption, loss of information, or other pecuniary loss) 
+arising out of use of this information, no matter the cause of action, 
+even if The MITRE Corporation has been advised of the possibility of such damages.
 EOF
 )
 
