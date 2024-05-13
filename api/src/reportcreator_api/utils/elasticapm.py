da3d7ce781cffe9a46ced84853bed9e3044fb4b3
Michael Wedl <michael@syslifters.com>
2024-05-13 09:53:34 +0200

    ElasticAPM: more ws tracing

--- api/src/reportcreator_api/utils/elasticapm.py
+++ api/src/reportcreator_api/utils/elasticapm.py
@@ -8,14 +8,22 @@ from django.urls import Resolver404, resolve
 from elasticapm.conf import constants
 from elasticapm.contrib.asgi import ASGITracingMiddleware
 from elasticapm.contrib.asyncio.traces import set_context
+from elasticapm.contrib.django.client import get_client
 from elasticapm.utils.disttracing import TraceParent
+
+
+def get_elasticapm_django_client():
+    try:
+        app = apps.get_app_config("elasticapm")
+        return app.client
+    except LookupError:
+        return get_client()
 
 
 class DjangoASGITracingMiddleware(ASGITracingMiddleware):
     @property
     def client(self):
-        app = apps.get_app_config("elasticapm")
-        return app.client
+        return get_elasticapm_django_client()
 
     @client.setter
     def client(self, _value):
@@ -31,7 +39,7 @@ class DjangoASGITracingMiddleware(ASGITracingMiddleware):
         elasticapm.set_transaction_name(f"{method} {resolver_match.route or resolver_match.url_name or path}")
 
 
-async def get_data_from_request(scope, event_type, event=None):
+def get_data_from_request(scope, event_type, event=None):
     m = DjangoASGITracingMiddleware(None)
 
     headers = m.get_headers(scope)
@@ -48,6 +56,17 @@ async def get_data_from_request(scope, event_type, event=None):
     return result
 
 
+def get_user_from_request(scope):
+    user = scope.get('user')
+    if user and not user.is_anonymous:
+        return {
+            'id': user.pk,
+            'username': user.username,
+        }
+    else:
+        return None
+
+
 def set_transaction_name_websocket(scope, event):
     resolver_match = resolve(scope.get('path'), urlconf=settings.WEBSOCKET_URLCONF)
     route = 'unknown'
@@ -59,15 +78,16 @@ def set_transaction_name_websocket(scope, event):
 
 
 @contextlib.asynccontextmanager
-async def elasticapm_capture_websocket_transaction(scope, event):
-    client = apps.get_app_config("elasticapm").client
+async def elasticapm_capture_websocket_transaction(scope, event=None):
+    client = get_elasticapm_django_client()
     client.begin_transaction(
         transaction_type="websocket",
         trace_parent=TraceParent.from_headers(scope["headers"]),
     )
     try:
         set_transaction_name_websocket(scope, event)
-        await set_context(lambda: get_data_from_request(scope, constants.TRANSACTION, event), "request")
+        await set_context(get_data_from_request(scope, constants.TRANSACTION, event), key="request")
+        await set_context(get_user_from_request(scope), key='user')
         yield
         elasticapm.set_transaction_outcome(constants.OUTCOME.SUCCESS, override=False)
     except Exception:
