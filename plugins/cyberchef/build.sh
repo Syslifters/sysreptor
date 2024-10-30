7eeb18f55c43db384f5f23ca91831e755599390a
Michael Wedl <michael@syslifters.com>
2024-10-30 10:48:02 +0100

    Plugin system bugfixes

--- plugins/cyberchef/build.sh
+++ plugins/cyberchef/build.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ ! -d static/cyberchef ]; then
+set -e
+
+CYBERCHEF_VERSION="v10.19.2"
+
+if [ ! -f "./static/cyberchef/CyberChef_${CYBERCHEF_VERSION}.html" ]; then
   echo "Downloading CyberChef"
+  rm -rf static/cyberchef/*
   mkdir -p static/cyberchef
-  curl https://github.com/gchq/CyberChef/releases/download/v10.19.2/CyberChef_v10.19.2.zip -o cyberchef.zip
+  curl -L "https://github.com/gchq/CyberChef/releases/download/v10.19.2/CyberChef_${CYBERCHEF_VERSION}.zip" -o cyberchef.zip
   unzip cyberchef.zip -d static/cyberchef
   rm cyberchef.zip
 else
