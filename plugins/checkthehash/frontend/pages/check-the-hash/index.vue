64616687bbd71288b2821b6e1cd5e32591206532
Florian Lotz <florian@syslifters.com>
2024-11-11 08:05:28 +0100

    Implement Feedback from Michi

--- plugins/checkthehash/frontend/pages/check-the-hash/index.vue
+++ plugins/checkthehash/frontend/pages/check-the-hash/index.vue
@@ -1,80 +1,63 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref, computed } from 'vue';
 import CheckTheHash from '~~/utils/CheckTheHash';
 import prototypes from '../../data/converted-prototypes.json';
 
 const hash = ref("");
-const results = ref<string[]>([]);
-const copiedIndex = ref<number | null>(null);
+const results = computed(() => {
+  const possibleHashes: string[] = CheckTheHash(hash.value, prototypes);
+  noResults.value = possibleHashes.length === 0 && hash.value.trim() !== ""; // Show message if no results
+  return possibleHashes;
+});
 const noResults = ref(false); // Flag for showing no results message
 
-function renderHash() {
-  const possibleHashes: string[] = CheckTheHash(hash.value, prototypes);
-  results.value = possibleHashes;
-  noResults.value = possibleHashes.length === 0 && hash.value.trim() !== ""; // Show message if no results
-}
-
-function copyToClipboard(text: string, index: number) {
+function copyToClipboard(text: string) {
   navigator.clipboard.writeText(text).then(() => {
-    copiedIndex.value = index;
-    setTimeout(() => {
-      copiedIndex.value = null;
-    }, 2000);
+    successToast('Copied');
   });
 }
-
-// Watch for changes in the hash and run renderHash when hash updates
-watch(hash, () => {
-  renderHash();
-});
 </script>
 
 <template>
-  <v-container class="d-flex flex-row align-center justify-center">
-    <v-row>
-      <v-col cols="12" class="d-flex flex-column align-center">
-        <!-- Text input triggers renderHash on input automatically -->
-        <v-text-field
-          v-model="hash"
-          label="Paste The Hash"
-          variant="underlined"
-          spellcheck="false"
-          hide-details="auto"
-          autofocus
-          class="mt-0 mb-2"
-          style="width: 50vw;"
-        />
-      </v-col>
+  <full-height-page class="scrollbar">
+    <v-container class="pt-5">
+      <v-row>
+        <v-col cols="12" class="d-flex flex-column align-center">
+          <!-- Text input triggers renderHash on input automatically -->
+          <v-text-field v-model="hash" label="Paste The Hash" variant="underlined" spellcheck="false"
+            hide-details="auto" autofocus class="mt-0 mb-2" style="width: 50vw;" />
+        </v-col>
 
-      <v-col cols="12" class="d-flex flex-column align-center">
-        <!-- Display "No results found" message when no matching hashes -->
-        <v-subheader v-if="noResults" class="text-h5 text-error mb-4">No matching hash mode found.</v-subheader>
+        <v-col cols="12" class="d-flex flex-column align-center">
+          <!-- Display "No results found" message when no matching hashes -->
+          <v-subheader v-if="noResults" class="text-h5 text-error mb-4">No matching hash mode found.</v-subheader>
 
-        <!-- Display possible hashes if there are any -->
-        <v-subheader v-if="results.length" class="text-h5 mb-4">Possible Hash Modes:</v-subheader>
-        
-        <v-list v-if="results.length" style="max-height: 70vh; width: 100%;">
-          <v-list-item
-            v-for="(result, index) in results"
-            :key="index"
-            class="mb-2 list-item"
-            @click="copyToClipboard(result, index)"
-          >
-            <v-list-item-content>
-              <v-list-item-title>{{ result }} <v-icon>mdi-content-copy</v-icon></v-list-item-title>
-            </v-list-item-content>
-            <v-list-item-content v-if="copiedIndex === index" class="ml-2">
-              <v-list-item-title class="text-success">Copied</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
+          <!-- Display possible hashes if there are any -->
+          <v-subheader v-if="results.length" class="text-h5 mb-4">Possible Hash Modes:</v-subheader>
 
-      </v-col>
-    </v-row>
-  </v-container>
+          <v-list v-if="results.length" class="pt-0 overflow-visible" style="width: 100%;">
+            <v-list-item v-for="(result, index) in results" :key="index" class="mb-2 list-item"
+              @click="copyToClipboard(result)">
+              <v-list-item-content>
+                <v-list-item :title="result" prepend-icon="mdi-content-copy"></v-list-item>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-col>
+      </v-row>
+    </v-container>
+    <footer class="footer">
+      <p>Identification based on: <a href="https://github.com/noraj/haiti" target="_blank">noraj/haiti</a></p>
+    </footer>
+  </full-height-page>
 </template>
 
 <style scoped>
+.scrollbar {
+  max-height: 100vh;
+  overflow-y: auto;
+}
+
 .list-item {
   animation: slide-in 0.5s ease-out;
   cursor: pointer;
@@ -92,9 +75,29 @@ watch(hash, () => {
     transform: translateY(20px);
     opacity: 0;
   }
+
   to {
     transform: translateY(0);
     opacity: 1;
   }
+}
+
+.footer {
+  text-align: center;
+  padding: 1rem;
+  background-color: #353535;
+  border-top: 1px solid #1d1d1d;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+
+.footer a {
+  color: #007bff;
+  text-decoration: none;
+}
+
+.footer a:hover {
+  text-decoration: underline;
 }
 </style>
