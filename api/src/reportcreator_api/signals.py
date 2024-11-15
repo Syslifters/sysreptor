c851094b4d4f3a2670f31e1304aa07ddf55b1844
Michael Wedl <michael@syslifters.com>
2024-11-15 10:19:09 +0100

    Send custom signals

---
+++ api/src/reportcreator_api/signals.py
@@ -0,0 +1,8 @@
+from django.db.models.signals import post_delete as django_post_delete
+from django.dispatch import Signal
+
+post_create = Signal()
+post_finish = Signal()
+post_archive = Signal()
+post_delete = django_post_delete
+
