542d0feadc91a5078ffcc6098234137e3629e65c
Michael Wedl <michael@syslifters.com>
2025-01-21 08:50:52 +0100

    Fix start.sh script for nounset

--- api/start.sh
+++ api/start.sh
@@ -6,7 +6,7 @@ set -o nounset
 set -o pipefail
 
 # Add custom CA certificates
-if [[ -n "$CA_CERTIFICATES" ]]; then
+if [[ -n "${CA_CERTIFICATES:-}" ]]; then
     echo "${CA_CERTIFICATES}" >> /usr/local/share/ca-certificates/custom-user-cert.crt
     update-ca-certificates
 fi
@@ -18,7 +18,7 @@ python3 manage.py collectstatic --noinput --no-post-process
 # Start web application
 gunicorn --bind=:8000 \
          --worker-class=uvicorn.workers.UvicornWorker \
-         --workers=${SERVER_WORKERS} \
+         --workers=${SERVER_WORKERS:-4} \
          --max-requests=500 \
          --max-requests-jitter=100 \
          --graceful-timeout=300 \
