2a225b29fb014e8d00031cd67842aaf496cff4bd
Michael Wedl <michael@syslifters.com>
2025-03-03 15:31:54 +0100

    Update signal usage

--- plugins/demoplugin/signals.py
+++ plugins/demoplugin/signals.py
@@ -1,8 +1,8 @@
 
 import logging
 
-from django.db.models import signals
 from django.dispatch import receiver
+from reportcreator_api import signals as sysreptor_signals
 from reportcreator_api.pentests.models.project import PentestProject
 
 log = logging.getLogger(__name__)
@@ -11,12 +11,12 @@ log = logging.getLogger(__name__)
 # https://docs.djangoproject.com/en/5.1/topics/signals/
 
 
-@receiver(signals.post_save, sender=PentestProject)
-def on_project_saved(sender, instance, created, **kwargs):
+@receiver(sysreptor_signals.post_update, sender=PentestProject)
+def on_project_saved(sender, instance, changed_fields, *args, **kwargs):
     """
     Signal handler for project save event.
     """
-    if not created and 'name' in instance.changed_fields:
+    if 'name' in changed_fields:
         old_name, new_name = instance.get_field_diff('name')
         log.info(f'Someone renamed project "{old_name}" to "{new_name}"')
 
