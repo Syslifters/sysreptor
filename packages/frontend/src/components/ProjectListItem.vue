8a1a21ba9004839efafd2bd6e4ce9c58e47e0dc8
aron <aron@syslifters.com>
2025-07-17 10:10:32 +0000

    Add shortcuts to project settings, notes, publish

--- packages/frontend/src/components/ProjectListItem.vue
+++ packages/frontend/src/components/ProjectListItem.vue
@@ -1,6 +1,52 @@
 <template>
-  <v-list-item :to="`/projects/${props.item.id}/reporting/`" lines="two">
-    <v-list-item-title>{{ props.item.name }}</v-list-item-title>
+  <v-list-item 
+    :to="`/projects/${props.item.id}/reporting/`" 
+    lines="two"
+    class="project-list-item"
+  >
+    
+    <v-list-item-title class="ms-2">
+      {{ props.item.name }}
+      <div class="action-buttons d-inline-flex ml-2">
+        <v-tooltip location="top" text="Settings" open-delay="500">
+          <template #activator="{ props: tooltipProps }">
+            <v-btn
+              v-bind="tooltipProps"
+              variant="text"
+              size="x-small"
+              icon="mdi-cogs"
+              :to="`/projects/${props.item.id}/`"
+              @click.stop
+            />
+          </template>
+        </v-tooltip>
+        
+        <v-tooltip location="top" text="Notes" open-delay="500">
+          <template #activator="{ props: tooltipProps }">
+            <v-btn
+              v-bind="tooltipProps"
+              variant="text"
+              size="x-small"
+              icon="mdi-notebook"
+              :to="`/projects/${props.item.id}/notes/`"
+              @click.stop
+            />
+          </template>
+        </v-tooltip>
+        <v-tooltip location="top" text="Publish" open-delay="500">
+          <template #activator="{ props: tooltipProps }">
+            <v-btn
+              v-bind="tooltipProps"
+              variant="text"
+              size="x-small"
+              icon="mdi-earth"
+              :to="`/projects/${props.item.id}/publish/`"
+              @click.stop
+            />
+          </template>
+        </v-tooltip>
+      </div>
+    </v-list-item-title>
 
     <v-list-item-subtitle class="mt-1">
       <chip-created :value="props.item.created" />
@@ -36,3 +82,14 @@ const emit = defineEmits<{
   filter: [filter: FilterValue];
 }>();
 </script>
+
+<style scoped>
+.project-list-item .action-buttons {
+  opacity: 0;
+  transition: opacity 0.2s ease-in-out;
+}
+
+.project-list-item:hover .action-buttons {
+  opacity: 1;
+}
+</style>
