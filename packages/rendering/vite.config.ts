017d2eb64de28c013aa5e6ad6ca59dadcd85d3b2
aron <aron@syslifters.com>
2025-07-18 08:07:50 +0000

    Remove NOTICEs from demo designs

--- packages/rendering/vite.config.ts
+++ packages/rendering/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     cssInjectedByJsPlugin(),
     viteStaticCopy({
       targets: [{
-        src: ['NOTICE', 'NOTICE_DESIGNS'],
+        src: ['NOTICE'],
         dest: '',
       }],
     }),
