3e8f2948863db2a383ca73d77e6ae6d4cdbc3133
aron <aron@syslifters.com>
2025-07-18 10:12:10 +0000

    Remove static copy

--- packages/rendering/vite.config.ts
+++ packages/rendering/vite.config.ts
@@ -2,19 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 
 export default defineConfig({
   plugins: [
     vue(),
     cssInjectedByJsPlugin(),
-    viteStaticCopy({
-      targets: [{
-        src: ['NOTICE'],
-        dest: '',
-      }],
-    }),
   ],
   resolve: {
     alias: {
