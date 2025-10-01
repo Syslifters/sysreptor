da5e3a80f25469c95ab196deffb777461ce79b0c
Michael Wedl <michael@syslifters.com>
2025-10-01 09:06:57 +0200

    Suggest combobox values in design preview data

--- packages/frontend/src/components/Design/PreviewDataForm.vue
+++ packages/frontend/src/components/Design/PreviewDataForm.vue
@@ -60,6 +60,7 @@
             @update:model-value="updateFindingField(fieldDefinition.id, $event)"
             :id="fieldDefinition.id"
             :definition="fieldDefinition"
+            :field-value-suggestions="findingFieldValueSuggestions"
             v-bind="fieldAttrs"
           >
             <template #markdown-context-menu="{value, definition, disabled}">
@@ -202,6 +203,8 @@ function setFieldDefinitionDefault(definition: FieldDefinition, value: any) {
   definition.default = value;
 }
 
+const findingsMap = computed(() => Object.fromEntries(findings.value.map(f => [f.id, f])));
+const findingFieldValueSuggestions = useFindingFieldValueSuggestions(findingsMap, props.projectType);
 const fieldAttrs = computed(() => ({
   showFieldIds: true,
   uploadFile: props.uploadFile,
