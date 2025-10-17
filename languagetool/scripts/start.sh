68cfa6dbcc656e6f9384022c59b8539374887b2e
Michael Wedl <michael@syslifters.com>
2025-10-17 09:42:26 +0200

    Update languagetool build

--- languagetool/scripts/start.sh
+++ languagetool/scripts/start.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 # Default options
 # https://languagetool.org/development/api/org/languagetool/server/HTTPServerConfig.html
-languagetool_cacheSize=${languagetool_cacheSize:-1000}
-languagetool_cacheTTLSeconds=${languagetool_cacheTTLSeconds:-300}
-languagetool_pipelineCaching=${languagetool_pipelineCaching:-true}
-languagetool_localApiMode=${languagetool_localApiMode:-true}
+export languagetool_cacheSize=${languagetool_cacheSize:-1000}
+export languagetool_cacheTTLSeconds=${languagetool_cacheTTLSeconds:-300}
+export languagetool_pipelineCaching=${languagetool_pipelineCaching:-true}
+export languagetool_localApiMode=${languagetool_localApiMode:-true}
 
-languagetool_dbDriver=${languagetool_dbDriver:-org.postgresql.Driver}
-languagetool_dbPort=${languagetool_dbPort:-5432}
-languagetool_dbUrl=jdbc:postgresql://${languagetool_dbHost}:${languagetool_dbPort}/${languagetool_dbName}
+export languagetool_dbDriver=${languagetool_dbDriver:-org.postgresql.Driver}
+export languagetool_dbPort=${languagetool_dbPort:-5432}
+export languagetool_dbUrl=jdbc:postgresql://${languagetool_dbHost}:${languagetool_dbPort}/${languagetool_dbName}
 unset languagetool_dbHost languagetool_dbPort languagetool_dbName
 
 Java_Xms=${Java_Xms:-256m}
@@ -18,9 +18,8 @@ Java_Xmx=${Java_Xmx:-2g}
 
 
 # Add languagetool options to config file
-for varname in ${!languagetool_*}
-do
-  echo "${varname#'languagetool_'}="${!varname} >> config.properties
+env | grep '^languagetool_' | while IFS='=' read -r key value; do
+  echo "${key#languagetool_}=${value}" >> config.properties
 done
 
 
