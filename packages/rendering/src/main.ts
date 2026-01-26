4d7bc9a527a2390a5126451c0e934561cad074ac
Michael Wedl <michael@syslifters.com>
2026-01-26 07:56:56 +0100

    Trigger vue rendering via function call

--- packages/rendering/src/main.ts
+++ packages/rendering/src/main.ts
@@ -52,184 +52,188 @@ export type ReportData = {
 
 declare global {
   interface Window {
-    RENDERING_COMPLETED: boolean;
-    REPORT_TEMPLATE: string;
-    REPORT_DATA: ReportData;
+    RENDERING_COMPLETED?: boolean;
+    REPORT_TEMPLATE?: string;
+    REPORT_DATA?: ReportData;
+
+    renderReportTemplate?: () => void;
   }
 }
 
 
-// injected as global variables
-const REPORT_TEMPLATE = '<div>' + (window.REPORT_TEMPLATE || '') + '</div>';
-const REPORT_DATA = window.REPORT_DATA || { report: {}, finding_groups: [], pentesters: [] };
+export function renderReportTemplate() {
+  // injected as global variables
+  const REPORT_TEMPLATE = '<div>' + (window.REPORT_TEMPLATE || '') + '</div>';
+  const REPORT_DATA = window.REPORT_DATA || { report: {}, finding_groups: [], pentesters: [] };
 
 
-const templateCompilerOptions = {
-  whitespace: 'preserve',
-  isRawTextTag: (tag: string) => ['markdown', 'mermaid-diagram', 'math-latex'].includes(tag),
-  isCustomElement: (tag: string) => ['footnote'].includes(tag),
-  comments: true,
-  ssr: false,
-  onError: (err: any) => {
-    const error = {
-      message: 'Template compilation error: ' + err.message,
-      details: err.loc && generateCodeFrame(REPORT_TEMPLATE, err.loc.start.offset, err.loc.end.offset),
-    };
-    console.error(error.message, error);
-    window.RENDERING_COMPLETED = true;
-  }
-} as const;
-const RENDER_FUNCTION = compile(REPORT_TEMPLATE, templateCompilerOptions);
+  const templateCompilerOptions = {
+    whitespace: 'preserve',
+    isRawTextTag: (tag: string) => ['markdown', 'mermaid-diagram', 'math-latex'].includes(tag),
+    isCustomElement: (tag: string) => ['footnote'].includes(tag),
+    comments: true,
+    ssr: false,
+    onError: (err: any) => {
+      const error = {
+        message: 'Template compilation error: ' + err.message,
+        details: err.loc && generateCodeFrame(REPORT_TEMPLATE, err.loc.start.offset, err.loc.end.offset),
+      };
+      console.error(error.message, error);
+      window.RENDERING_COMPLETED = true;
+    }
+  } as const;
+  const RENDER_FUNCTION = compile(REPORT_TEMPLATE, templateCompilerOptions);
 
 
-// Skip rendering on template error
-if (!window.RENDERING_COMPLETED) {
-  const app = createApp(defineComponent({
-    name: 'root',
-    render: RENDER_FUNCTION,
-    components: { Pagebreak, Markdown, CommaAndJoin, TableOfContents, ListOfFigures, ListOfTables, Chart, MermaidDiagram, Qrcode, MathLatex, Ref },
-    setup() {
-      // Data
-      const data = REPORT_DATA;
-      const finding_groups = computed(() => data.finding_groups || [{label: '', findings: data.findings || []}]);
-      const findings = computed(() => lodash.sortBy(finding_groups.value.flatMap(g => g.findings), ['order']));
-      const pentesters = computed(() => data.pentesters);
-      const report = computed(() => ({ ...data.report, findings: findings.value }));
+  // Skip rendering on template error
+  if (!window.RENDERING_COMPLETED) {
+    const app = createApp(defineComponent({
+      name: 'root',
+      render: RENDER_FUNCTION,
+      components: { Pagebreak, Markdown, CommaAndJoin, TableOfContents, ListOfFigures, ListOfTables, Chart, MermaidDiagram, Qrcode, MathLatex, Ref },
+      setup() {
+        // Data
+        const data = REPORT_DATA;
+        const finding_groups = computed(() => data.finding_groups || [{label: '', findings: data.findings || []}]);
+        const findings = computed(() => lodash.sortBy(finding_groups.value.flatMap(g => g.findings), ['order']));
+        const pentesters = computed(() => data.pentesters);
+        const report = computed(() => ({ ...data.report, findings: findings.value }));
 
-      // Finding stats
-      const findings_info = computed(() => findings.value.filter(f => (f.severity?.value || f.cvss?.level) === 'info'));
-      const findings_low = computed(() => findings.value.filter(f => (f.severity?.value || f.cvss?.level) === 'low'));
-      const findings_medium = computed(() => findings.value.filter(f => (f.severity?.value || f.cvss?.level) === 'medium'));
-      const findings_high = computed(() => findings.value.filter(f => (f.severity?.value || f.cvss?.level) === 'high'));
-      const findings_critical = computed(() => findings.value.filter(f => (f.severity?.value || f.cvss?.level) === 'critical'));
-      const finding_stats = computed(() => ({
-        count_total: findings.value.length,
-        count_critical: findings_critical.value.length,
-        count_high: findings_high.value.length,
-        count_medium: findings_medium.value.length,
-        count_low: findings_low.value.length,
-        count_info: findings_info.value.length,
-      }));
+        // Finding stats
+        const findings_info = computed(() => findings.value.filter(f => (f.severity?.value || f.cvss?.level) === 'info'));
+        const findings_low = computed(() => findings.value.filter(f => (f.severity?.value || f.cvss?.level) === 'low'));
+        const findings_medium = computed(() => findings.value.filter(f => (f.severity?.value || f.cvss?.level) === 'medium'));
+        const findings_high = computed(() => findings.value.filter(f => (f.severity?.value || f.cvss?.level) === 'high'));
+        const findings_critical = computed(() => findings.value.filter(f => (f.severity?.value || f.cvss?.level) === 'critical'));
+        const finding_stats = computed(() => ({
+          count_total: findings.value.length,
+          count_critical: findings_critical.value.length,
+          count_high: findings_high.value.length,
+          count_medium: findings_medium.value.length,
+          count_low: findings_low.value.length,
+          count_info: findings_info.value.length,
+        }));
 
-      // Wait until rendering is finished
-      const _tickCount = ref<number>(0);
-      const _observer = new MutationObserver((mutationList) => {
-        for (const mutation of mutationList) {
-          if (mutation.type === 'childList') {
-            for (const an of Array.from(mutation.addedNodes)) {
-              for (const node of getChildElementsRecursive(an)) {
-                if (node.nodeName === 'SCRIPT' && node.attributes.getNamedItem('src')?.value) {
-                  const waitScriptLoaded = useRenderTask(() => new Promise((resolve, reject) => {
-                    node.addEventListener('load', () => resolve());
-                    node.addEventListener('error', reject);
-                  }));
-                  waitScriptLoaded();
+        // Wait until rendering is finished
+        const _tickCount = ref<number>(0);
+        const _observer = new MutationObserver((mutationList) => {
+          for (const mutation of mutationList) {
+            if (mutation.type === 'childList') {
+              for (const an of Array.from(mutation.addedNodes)) {
+                for (const node of getChildElementsRecursive(an)) {
+                  if (node.nodeName === 'SCRIPT' && node.attributes.getNamedItem('src')?.value) {
+                    const waitScriptLoaded = useRenderTask(() => new Promise((resolve, reject) => {
+                      node.addEventListener('load', () => resolve());
+                      node.addEventListener('error', reject);
+                    }));
+                    waitScriptLoaded();
+                  }
                 }
               }
             }
           }
-        }
-      });
-      _observer.observe(document, { childList: true, subtree: true });
-      onBeforeUnmount(() => _observer.disconnect());
-      onMounted(async () => {
-        // Wait some ticks before rendering is signaled as completed
-        // Allow multi-pass rendering (for e.g. table of contents)
-        await callForTicks(10, () => {
-          _tickCount.value += 1;
         });
-        // Wait for all pending render tasks to finish
-        while (pendingRenderTasks.value.length > 0) {
-          const pending = [...pendingRenderTasks.value];
-          pendingRenderTasks.value = [];
-          await Promise.allSettled(pending);
-          // Wait some ticks to allow registering new render tasks
-          await callForTicks(10, () => { 
-            _tickCount.value += 1; 
+        _observer.observe(document, { childList: true, subtree: true });
+        onBeforeUnmount(() => _observer.disconnect());
+        onMounted(async () => {
+          // Wait some ticks before rendering is signaled as completed
+          // Allow multi-pass rendering (for e.g. table of contents)
+          await callForTicks(10, () => {
+            _tickCount.value += 1;
           });
+          // Wait for all pending render tasks to finish
+          while (pendingRenderTasks.value.length > 0) {
+            const pending = [...pendingRenderTasks.value];
+            pendingRenderTasks.value = [];
+            await Promise.allSettled(pending);
+            // Wait some ticks to allow registering new render tasks
+            await callForTicks(10, () => { 
+              _tickCount.value += 1; 
+            });
+          }
+
+          window.RENDERING_COMPLETED = true;
+        });
+
+        return {
+          data,
+          finding_groups,
+          findings,
+          pentesters,
+          report,
+          findings_info,
+          findings_low,
+          findings_medium,
+          findings_high,
+          findings_critical,
+          finding_stats,
+          // Provide libraries and utilities
+          chartjsPlugins: {
+            DataLabels: ChartJsPluginDataLabels,
+          },
+          lodash,
+          window,
+          document,
+          computed,
         }
-
-        window.RENDERING_COMPLETED = true;
-      });
-
-      return {
-        data,
-        finding_groups,
-        findings,
-        pentesters,
-        report,
-        findings_info,
-        findings_low,
-        findings_medium,
-        findings_high,
-        findings_critical,
-        finding_stats,
-        // Provide libraries and utilities
-        chartjsPlugins: {
-          DataLabels: ChartJsPluginDataLabels,
+      },
+      methods: {
+        capitalize(value?: string|null) {
+          if (!value) { return ''; }
+          value = value.toString()
+          return value.charAt(0).toUpperCase() + value.slice(1);
         },
-        lodash,
-        window,
-        document,
-        computed,
+        formatDate(date?: string|null, options: Intl.DateTimeFormatOptions|Intl.DateTimeFormatOptions['dateStyle']|'iso' = 'long', locales: Intl.LocalesArgument = undefined) {
+          if (!date) {
+            return '';
+          }
+          const dateObj = new Date(date);
+          if (options === 'iso') {
+            return new Date(dateObj.getTime() - (dateObj.getTimezoneOffset() * 60000)).toISOString().split("T")[0];
+          }
+      
+          if (typeof options === 'string') {
+            options = { dateStyle: options };
+          }
+          if (!locales) {
+            // Get locale from <html lang="...">, since this locale is not automatically applied when formatting dates 
+            // (at least in headless chrome, which has poor locale support)
+            locales = document.documentElement.lang;
+          }
+          return dateObj.toLocaleDateString(locales, options);
+        },
+        cssvar(name: string) {
+          const value = window.getComputedStyle(document.documentElement).getPropertyValue(name);
+          if (!value) {
+            console.warn({
+              message: 'CSS variable not defined',
+              details: `The CSS variable referenced via cssvar("${name}") does not exist.`,
+            });
+          }
+          return value;
+        },
+      },
+    }));
+    app.config.compilerOptions = templateCompilerOptions;
+    app.config.warnHandler = (msg, _instance, trace) => {
+      if (msg === 'Avoid app logic that relies on enumerating keys on a component instance. The keys will be empty in production mode to avoid performance overhead.') {
+        return;
       }
-    },
-    methods: {
-      capitalize(value?: string|null) {
-        if (!value) { return ''; }
-        value = value.toString()
-        return value.charAt(0).toUpperCase() + value.slice(1);
-      },
-      formatDate(date?: string|null, options: Intl.DateTimeFormatOptions|Intl.DateTimeFormatOptions['dateStyle']|'iso' = 'long', locales: Intl.LocalesArgument = undefined) {
-        if (!date) {
-          return '';
-        }
-        const dateObj = new Date(date);
-        if (options === 'iso') {
-          return new Date(dateObj.getTime() - (dateObj.getTimezoneOffset() * 60000)).toISOString().split("T")[0];
-        }
-    
-        if (typeof options === 'string') {
-          options = { dateStyle: options };
-        }
-        if (!locales) {
-          // Get locale from <html lang="...">, since this locale is not automatically applied when formatting dates 
-          // (at least in headless chrome, which has poor locale support)
-          locales = document.documentElement.lang;
-        }
-        return dateObj.toLocaleDateString(locales, options);
-      },
-      cssvar(name: string) {
-        const value = window.getComputedStyle(document.documentElement).getPropertyValue(name);
-        if (!value) {
-          console.warn({
-            message: 'CSS variable not defined',
-            details: `The CSS variable referenced via cssvar("${name}") does not exist.`,
-          });
-        }
-        return value;
-      },
-    },
-  }));
-  app.config.compilerOptions = templateCompilerOptions;
-  app.config.warnHandler = (msg, _instance, trace) => {
-    if (msg === 'Avoid app logic that relies on enumerating keys on a component instance. The keys will be empty in production mode to avoid performance overhead.') {
-      return;
-    }
-    const warning = {
-      message: msg,
-      details: trace
+      const warning = {
+        message: msg,
+        details: trace
+      };
+      console.warn(warning.message, warning);
     };
-    console.warn(warning.message, warning);
-  };
-  app.config.errorHandler = (err, _instance, info) => {
-    const error = {
-      message: (err as any).toString() as string,
-      details: 'Error in ' + info,
+    app.config.errorHandler = (err, _instance, info) => {
+      const error = {
+        message: (err as any).toString() as string,
+        details: 'Error in ' + info,
+      };
+      console.error(error.message, error);
+      window.RENDERING_COMPLETED = true;
     };
-    console.error(error.message, error);
-    window.RENDERING_COMPLETED = true;
-  };
-  app.mount('body');
+    app.mount('body');
+  }
 }
-
+window.renderReportTemplate = renderReportTemplate;
