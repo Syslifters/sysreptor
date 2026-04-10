fcc43cab686874d7089e1259fdf3e9083e624ebf
Michael Wedl <michael@syslifters.com>
2026-04-10 09:37:18 +0200

    Update docs about backup key encoding

--- api/src/sysreptor/management/commands/backup.py
+++ api/src/sysreptor/management/commands/backup.py
@@ -4,7 +4,6 @@ import logging
 from django.core.management.base import BaseCommand, CommandError, CommandParser
 
 from sysreptor.api_utils.backup_utils import create_backup, encrypt_backup, to_chunks
-from sysreptor.api_utils.models import BackupLog, BackupLogType
 from sysreptor.utils import crypto, license
 
 
@@ -18,7 +17,7 @@ def aes_key(val):
 class Command(BaseCommand):
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument('file', nargs='?', type=argparse.FileType('wb'), default='-')
-        parser.add_argument('--key', type=aes_key, help='AES key to encrypt the backup (optional)')
+        parser.add_argument('--key', type=aes_key, help='AES key as hex string to encrypt the backup (optional)')
 
     def handle(self, file, key, verbosity=1, **kwargs) -> str | None:
         if verbosity <= 0:
@@ -37,4 +36,3 @@ class Command(BaseCommand):
             for c in to_chunks(z):
                 file.write(c)
 
-        
