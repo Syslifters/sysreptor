94f2f5fe91c65bfcffc6838640f64c5557313207
Michael Wedl <michael@syslifters.com>
2026-05-05 09:35:21 +0200

    Check configuration and settings and log warnings

--- api/src/sysreptor/api_utils/apps.py
+++ api/src/sysreptor/api_utils/apps.py
@@ -5,3 +5,5 @@ class ApiUtilsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sysreptor.api_utils'
 
+    def ready(self):
+        from . import signals  # noqa
